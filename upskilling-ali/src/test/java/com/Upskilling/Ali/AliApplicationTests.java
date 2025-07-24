95fb8de3f4d5c16c8c56dafc0bd60ee28fdc2ee1
alialsubhi <alialsubhi1998@gmail.com>
2025-07-25 00:42:17 +0400

    Dummy project to start with

---
+++ upskilling-ali/src/test/java/com/Upskilling/Ali/AliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Upskilling.Ali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
